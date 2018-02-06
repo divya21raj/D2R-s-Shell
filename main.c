573e3162adb6f52dd6460c4e5ae993705159a483
Divya Raj <divya21raj@gmail.com>
2018-02-06 21:13:41 +0530

    '&' feature added

--- main.c
+++ main.c
@@ -13,21 +13,25 @@
 
 #define MAXLINE 80 /* The maximum length of a command*/
 
-int shouldRun = 1; /* flag indicating if exit was the input*/
-
 void tokenizeInput(char input[MAXLINE], char *args[MAXLINE/2 + 1]);
 
 int isInbuilt(char input[MAXLINE]);/*To check if command is inbuilt
- 									returns 1, 2, 0 :exit, cd, not inbuilt*/
-
+									returns 1, 2, 0 :exit, cd, not inbuilt*/
 void handleInbuilt(int inbuilt, char *args[MAXLINE/2 + 1]);
+
+void runCommand(char *args[41]);
+
+int checkAmpersand(char input[MAXLINE]);
+
+int shouldRun = 1; /* flag indicating if exit was the input*/
+int inbuilt = 0; /* flag indicating if command is inbuilt */
+int hasAmpersand = 0; /*flag indicating if input fas precede by &*/
+
 
 int main(void)
 {
 	char input[MAXLINE];
 	char *args[MAXLINE/2 + 1]; /* command line arguments */
-
-	int inbuilt = 0; /* flag indicating if command is inbuilt */
 
 	while (shouldRun)
 	{
@@ -39,50 +43,67 @@ int main(void)
 		input[strlen(input)-1] = '\0'; //shaving off the \n at the end
 
 		inbuilt = isInbuilt(input);
+		hasAmpersand = checkAmpersand(input);
 
 		tokenizeInput(input, args);
 
-		if(!inbuilt)
-		{
-			pid_t pid;
-
-			pid = fork(); /* fork a child process */
-
-			if (pid < 0)
-			{    /* error occurred */
-				fprintf(stderr, "Fork Failed");
-				return 1;
-			}
-			else if (pid == 0)
-			{    /* child process */
-				if (execvp(args[0], args) < 0)
-					printf("\nCould not execute command..");
-
-				exit(0);
-			}
-			else
-			{    /* parent process */
-				/* parent will wait for the child to complete */
-				wait(NULL);
-				printf("Child Complete\n");
-
-			}
-		}
-
-		else
-			handleInbuilt(inbuilt, args);
-
-		/**
-		* After reading user input, the steps are:
-		* (1) fork a child process using fork()
-	 	* * (2) the child process will invoke execvp()
-		* (3) if command included &, parent will not invoke wait()
-		* (4) if command is quit, the shell should exit
-		* Explain your steps as comments in the code itself.
-		*/
+		runCommand(args);
 	}
 
 	return 0;
+}
+
+int checkAmpersand(char input[MAXLINE])
+{
+	size_t size = strlen(input);
+
+	if(input[size-1] == '&')
+	{
+		input[size - 1] = '\0';
+		return 1;
+	}
+
+	else return 0;
+}
+
+void runCommand(char *args[MAXLINE/2 + 1])
+{
+	if(!inbuilt)
+	{
+		pid_t pid;
+
+		pid = fork(); /* fork a child process */
+
+		if (pid < 0)
+		{    /* error occurred */
+			fprintf(stderr, "Fork Failed");
+		}
+		else if (pid == 0)
+		{    /* child process */
+			if (execvp(args[0], args) < 0)
+				printf("\nCould not execute command..");
+
+			hasAmpersand = 0;
+			exit(0);
+		}
+		else
+		{    /* parent process */
+			/* parent will wait for the child to complete */
+			if(!hasAmpersand)
+				wait(NULL);
+
+			else
+			{
+				printf("Process created with PID: %d\n",pid);
+				sleep(1);
+			}
+
+		}
+	}
+
+	else
+		handleInbuilt(inbuilt, args);
+
 }
 
 void handleInbuilt(int inbuilt, char *args[MAXLINE/2 + 1])
